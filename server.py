742b6a086bda6c23092223a8d1d9892b35700020
Abdullah Al-Ahmadi <abdu250@gmail.com>
2023-01-15 09:33:33 +0300

    another commit

--- server.py
+++ server.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+import tensorflow as tf
+import numpy as np
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-    return "Homepage"
+    return "Homepages"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    model = tf.keras.models.load_model('model')
+    data = request.form.get('data')
+    prediction = model.predict(data)
+    score = tf.nn.softmax(prediction[0])
+    return score
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
